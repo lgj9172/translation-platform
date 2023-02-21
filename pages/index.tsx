f4ca0a01230514026d02465a005cd9ba18bca137
GJ Lee <lgj9172@gmail.com>
2023-02-21 18:57:16 +0900

    add: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import MainLayout from "@/layouts/MainLayout";
+import { Anchor, Stack } from "@mantine/core";
+import Link from "next/link";
 
 export default function Index() {
-  return <div />;
+  return (
+    <Stack spacing="xs">
+      <Link href="/sign-in" passHref legacyBehavior>
+        <Anchor>로그인 페이지</Anchor>
+      </Link>
+      <Link href="/sign-up" passHref legacyBehavior>
+        <Anchor>회원가입 페이지</Anchor>
+      </Link>
+      <Link href="/find-account" passHref legacyBehavior>
+        <Anchor>이메일/비밀번호 찾기</Anchor>
+      </Link>
+      <Link href="/renew-password" passHref legacyBehavior>
+        <Anchor>비밀번호 재설정 페이지</Anchor>
+      </Link>
+    </Stack>
+  );
 }
 
 Index.getLayout = MainLayout;
