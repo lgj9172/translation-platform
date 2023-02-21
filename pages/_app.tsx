548e822255c23c259ed12fba135f916d53d19b48
GJ Lee <lgj9172@gmail.com>
2023-02-21 18:55:38 +0900

    add: global configure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { MantineProvider } from "@mantine/core";
+import { ModalsProvider } from "@mantine/modals";
+import { NotificationsProvider } from "@mantine/notifications";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ReactElement, ReactNode } from "react";
 
 export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<P, IP> & {
@@ -21,9 +24,19 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       theme={{
         /** Put your mantine theme override here */
         colorScheme: "light",
+        fontFamily: "'Noto Sans KR', sans-serif",
+        fontFamilyMonospace: "Monaco, Courier, monospace",
+        headings: { fontFamily: "'Noto Sans KR', sans-serif" },
       }}
     >
-      {getLayout(<Component {...pageProps} />)}
+      <Head>
+        <title>Fluence</title>
+      </Head>
+      <ModalsProvider>
+        <NotificationsProvider position="top-right" limit={2}>
+          {getLayout(<Component {...pageProps} />)}
+        </NotificationsProvider>
+      </ModalsProvider>
     </MantineProvider>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,15 @@ export default class Doc extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
