10782be1b15baf7a9465d7778a8c466901c00ef0
GJ <lgj9172@gmail.com>
2023-04-21 00:31:13 +0900

    add: main page mockup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,292 @@
 import MainLayout from "@/layouts/MainLayout";
-import { Anchor, Stack } from "@mantine/core";
-import Link from "next/link";
+import { Carousel } from "@mantine/carousel";
+import { Badge, Card, Flex, Group, Select, SimpleGrid, Space, Stack, Text, Title, UnstyledButton } from "@mantine/core";
+import { IconHeart, IconSend } from "@tabler/icons";
 
 export default function Index() {
   return (
     <Stack spacing="xs">
-      <Link href="/sign-in" passHref legacyBehavior>
-        <Anchor>로그인 페이지</Anchor>
-      </Link>
-      <Link href="/sign-up" passHref legacyBehavior>
-        <Anchor>회원가입 페이지</Anchor>
-      </Link>
-      <Link href="/find-account" passHref legacyBehavior>
-        <Anchor>이메일 비밀번호 찾기</Anchor>
-      </Link>
-      <Link href="/renew-password" passHref legacyBehavior>
-        <Anchor>비밀번호 재설정 페이지</Anchor>
-      </Link>
-      <Link href="/update-account" passHref legacyBehavior>
-        <Anchor>프로필 수정 페이지</Anchor>
-      </Link>
+      <Carousel height={200} slideGap="md" controlsOffset="md" loop withIndicators>
+        <Carousel.Slide bg="gray">
+          <Flex h="100%" align="center" justify="center">
+            Fluence
+          </Flex>
+        </Carousel.Slide>
+        <Carousel.Slide bg="gray">
+          <Flex h="100%" align="center" justify="center">
+            Fluence
+          </Flex>
+        </Carousel.Slide>
+        <Carousel.Slide bg="gray">
+          <Flex h="100%" align="center" justify="center">
+            Fluence
+          </Flex>
+        </Carousel.Slide>
+      </Carousel>
+
+      <Space h="xl" />
+
+      <Stack>
+        <Title>번역</Title>
+
+        <SimpleGrid cols={4} breakpoints={[{ maxWidth: "xs", cols: 2 }]}>
+          <Select
+            placeholder="언어"
+            data={[
+              { value: "en_ko", label: "영한" },
+              { value: "ko_en", label: "한영" },
+            ]}
+          />
+          <Select
+            placeholder="마감일"
+            data={[
+              { value: "today", label: "오늘" },
+              { value: "week", label: "이번주" },
+            ]}
+          />
+          <Select
+            placeholder="번역료"
+            data={[
+              { value: "10000", label: "만원이상" },
+              { value: "100000", label: "십만원이상" },
+            ]}
+          />
+          <Select placeholder="분야" data={[{ value: "it", label: "IT" }]} />
+        </SimpleGrid>
+
+        <Stack>
+          <UnstyledButton>
+            <Card h={200} shadow="sm" p={0} radius="md" withBorder>
+              <Group h="100%" w="100%" noWrap>
+                <Stack w={80} h="100%" bg="blue" align="center" justify="center" spacing={0}>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    2/15
+                  </Text>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    15:00
+                  </Text>
+                </Stack>
+                <Stack style={{ flexGrow: 1 }}>
+                  <Group>
+                    <Badge color="green">한영</Badge>
+                    <Badge color="blue">IT</Badge>
+                    <Badge color="red">PPT</Badge>
+                  </Group>
+                  <Stack spacing={0}>
+                    <Text fz="xl" fw={700}>
+                      번역 요청 부탁드립니다.
+                    </Text>
+                    <Text fz="md" fw={500}>
+                      발표용 자료로 되도록 빨리 필요합니다.
+                    </Text>
+                  </Stack>
+                  <Group position="apart">
+                    <Stack spacing={0}>
+                      <Text fz="xs">1,200글자</Text>
+                      <Text fz="xl" fw={700}>
+                        50,000원
+                      </Text>
+                    </Stack>
+                    <Group>
+                      <Group spacing={4}>
+                        <IconSend />4
+                      </Group>
+                      <Group spacing={4}>
+                        <IconHeart />5
+                      </Group>
+                    </Group>
+                  </Group>
+                </Stack>
+                <Space />
+              </Group>
+            </Card>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Card h={200} shadow="sm" p={0} radius="md" withBorder>
+              <Group h="100%" w="100%" noWrap>
+                <Stack w={80} h="100%" bg="blue" align="center" justify="center" spacing={0}>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    2/15
+                  </Text>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    15:00
+                  </Text>
+                </Stack>
+                <Stack style={{ flexGrow: 1 }}>
+                  <Group>
+                    <Badge color="green">한영</Badge>
+                    <Badge color="blue">IT</Badge>
+                    <Badge color="red">PPT</Badge>
+                  </Group>
+                  <Stack spacing={0}>
+                    <Text fz="xl" fw={700}>
+                      번역 요청 부탁드립니다.
+                    </Text>
+                    <Text fz="md" fw={500}>
+                      발표용 자료로 되도록 빨리 필요합니다.
+                    </Text>
+                  </Stack>
+                  <Group position="apart">
+                    <Stack spacing={0}>
+                      <Text fz="xs">1,200글자</Text>
+                      <Text fz="xl" fw={700}>
+                        50,000원
+                      </Text>
+                    </Stack>
+                    <Group>
+                      <Group spacing={4}>
+                        <IconSend />4
+                      </Group>
+                      <Group spacing={4}>
+                        <IconHeart />5
+                      </Group>
+                    </Group>
+                  </Group>
+                </Stack>
+                <Space />
+              </Group>
+            </Card>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Card h={200} shadow="sm" p={0} radius="md" withBorder>
+              <Group h="100%" w="100%" noWrap>
+                <Stack w={80} h="100%" bg="blue" align="center" justify="center" spacing={0}>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    2/15
+                  </Text>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    15:00
+                  </Text>
+                </Stack>
+                <Stack style={{ flexGrow: 1 }}>
+                  <Group>
+                    <Badge color="green">한영</Badge>
+                    <Badge color="blue">IT</Badge>
+                    <Badge color="red">PPT</Badge>
+                  </Group>
+                  <Stack spacing={0}>
+                    <Text fz="xl" fw={700}>
+                      번역 요청 부탁드립니다.
+                    </Text>
+                    <Text fz="md" fw={500}>
+                      발표용 자료로 되도록 빨리 필요합니다.
+                    </Text>
+                  </Stack>
+                  <Group position="apart">
+                    <Stack spacing={0}>
+                      <Text fz="xs">1,200글자</Text>
+                      <Text fz="xl" fw={700}>
+                        50,000원
+                      </Text>
+                    </Stack>
+                    <Group>
+                      <Group spacing={4}>
+                        <IconSend />4
+                      </Group>
+                      <Group spacing={4}>
+                        <IconHeart />5
+                      </Group>
+                    </Group>
+                  </Group>
+                </Stack>
+                <Space />
+              </Group>
+            </Card>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Card h={200} shadow="sm" p={0} radius="md" withBorder>
+              <Group h="100%" w="100%" noWrap>
+                <Stack w={80} h="100%" bg="blue" align="center" justify="center" spacing={0}>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    2/15
+                  </Text>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    15:00
+                  </Text>
+                </Stack>
+                <Stack style={{ flexGrow: 1 }}>
+                  <Group>
+                    <Badge color="green">한영</Badge>
+                    <Badge color="blue">IT</Badge>
+                    <Badge color="red">PPT</Badge>
+                  </Group>
+                  <Stack spacing={0}>
+                    <Text fz="xl" fw={700}>
+                      번역 요청 부탁드립니다.
+                    </Text>
+                    <Text fz="md" fw={500}>
+                      발표용 자료로 되도록 빨리 필요합니다.
+                    </Text>
+                  </Stack>
+                  <Group position="apart">
+                    <Stack spacing={0}>
+                      <Text fz="xs">1,200글자</Text>
+                      <Text fz="xl" fw={700}>
+                        50,000원
+                      </Text>
+                    </Stack>
+                    <Group>
+                      <Group spacing={4}>
+                        <IconSend />4
+                      </Group>
+                      <Group spacing={4}>
+                        <IconHeart />5
+                      </Group>
+                    </Group>
+                  </Group>
+                </Stack>
+                <Space />
+              </Group>
+            </Card>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Card h={200} shadow="sm" p={0} radius="md" withBorder>
+              <Group h="100%" w="100%" noWrap>
+                <Stack w={80} h="100%" bg="blue" align="center" justify="center" spacing={0}>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    2/15
+                  </Text>
+                  <Text fz="xl" fw={700} color="white" align="center">
+                    15:00
+                  </Text>
+                </Stack>
+                <Stack style={{ flexGrow: 1 }}>
+                  <Group>
+                    <Badge color="green">한영</Badge>
+                    <Badge color="blue">IT</Badge>
+                    <Badge color="red">PPT</Badge>
+                  </Group>
+                  <Stack spacing={0}>
+                    <Text fz="xl" fw={700}>
+                      번역 요청 부탁드립니다.
+                    </Text>
+                    <Text fz="md" fw={500}>
+                      발표용 자료로 되도록 빨리 필요합니다.
+                    </Text>
+                  </Stack>
+                  <Group position="apart">
+                    <Stack spacing={0}>
+                      <Text fz="xs">1,200글자</Text>
+                      <Text fz="xl" fw={700}>
+                        50,000원
+                      </Text>
+                    </Stack>
+                    <Group>
+                      <Group spacing={4}>
+                        <IconSend />4
+                      </Group>
+                      <Group spacing={4}>
+                        <IconHeart />5
+                      </Group>
+                    </Group>
+                  </Group>
+                </Stack>
+                <Space />
+              </Group>
+            </Card>
+          </UnstyledButton>
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
